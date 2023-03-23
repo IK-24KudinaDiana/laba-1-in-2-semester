67882f804f4d4ffa3233d6bc7ffdc7fe74b522a6
IK-24KudinaDiana <kudinadiana91@gmail.com>
2023-03-23 14:23:05 +0200

    2

---
+++ лаба-1/laba-1-2/laba-1-2/Program.cs
@@ -0,0 +1,71 @@
+﻿//Написати програму згідно отриманого завдання використовуючи словники Dictionary в C#.
+//	Якщо результатом виконання програми є словник, зберегти цей результат у JSON файл
+//Дано словник.
+//	Вивести тільки ті позиції словника, в яких ключ більший або дорівнює заданому значенню. 
+//	Повернути null, якщо такого ключа не існує.Записати у JSON
+using Newtonsoft.Json;
+do
+{
+	Console.Clear();
+	int n;
+	if (TryInputValue("Введiть значення: ", out n))
+	{
+		Dictionary<int, string> MyDictionary = new Dictionary<int, string>()
+		{
+			{ 1, "The moon" },
+			{ 2, "Earth" },
+			{ 3, "Sun " },
+			{ 4, "Mars" }
+		};
+		Dictionary<int, string> MyDictionary1 = new Dictionary<int, string>();
+
+		KeyVerification(MyDictionary, n);
+		ActionsWithTheDictionary(MyDictionary, ref MyDictionary1, n);
+		Json(MyDictionary1);
+
+	}
+
+	Console.WriteLine("\n You want to complete the program: yes or no");
+} while (!(string.Equals(Console.ReadLine(), "yes", StringComparison.OrdinalIgnoreCase)));
+static bool TryInputValue(string argumentName, out int value)
+{
+	Console.Write($"{argumentName}");
+	string stringValue = Console.ReadLine();
+	if (int.TryParse(stringValue, out value))
+	{
+		return true;
+	}
+	Console.WriteLine($"You wrote incorrect value {stringValue}");
+	return false;
+}
+
+static void Json(Dictionary<int, string> MyDictionary1)
+{
+	string file = "E:\\Навчання\\1.txt";
+	string json = JsonConvert.SerializeObject(MyDictionary1);
+	StreamWriter sw = new StreamWriter(file);
+	sw.WriteLine(json);
+	sw.Close();
+}
+static void ActionsWithTheDictionary(Dictionary<int, string> MyDictionary, ref Dictionary<int, string> MyDictionary1, int n)
+{
+	int k = 1;
+	foreach (var key in MyDictionary)
+	{
+		if (k >= n)
+		{
+			Console.WriteLine($"key: {key.Key}  value: {key.Value}");
+			MyDictionary1.Add(key.Key, key.Value);
+		}
+		k++;
+	}
+}
+static void KeyVerification(Dictionary<int, string> MyDictionary, int n)
+{
+	bool DictionaryKey = MyDictionary.ContainsKey(n);
+	if (DictionaryKey == false)
+	{
+		Console.WriteLine("null");
+	}
+	//Console.WriteLine($"Key: {DictionaryKey}");
+}
---
+++ лаба-1/laba-1-3/laba-1-3/Program.cs
@@ -0,0 +1,59 @@
+﻿//Написати програму згідно отриманого завдання використовуючи лише LINQ методи.
+//	В кінці завдання в дужках наведена підказка, які методи LINQ могли б вам
+//	допомогти у розв'язанні задачі
+
+//Дана цифра D (ціле однозначне число) і послідовність цілих чисел A.
+//	Витягти з A всі різні додатні числа, що закінчуються цифрою D (в вихідному порядку).
+//	При наявності повторюваних елементів видаляти всі їх входження, крім останніх. 
+//	Порада: Послідовно застосувати методи Reverse, Distinct, Reverse. (2)
+
+
+do
+{
+	Console.Clear();
+	int number;
+	int[] myarray = { 1, 25, 35, -45, 65, 23, -85, 95, 25, 95 };
+	if (TryInputValue("Enter a number between 0 and 9: ", out number))
+	{
+		ActionsWithArray(myarray);
+		LINQ(myarray, number);
+	}
+
+	Console.WriteLine("\n You want to complete the program: yes or no");
+} while (!(string.Equals(Console.ReadLine(), "yes", StringComparison.OrdinalIgnoreCase)));
+
+static bool TryInputValue(string argumentName, out int value)
+{
+	Console.Write($"{argumentName}");
+	string stringValue = Console.ReadLine();
+	if (int.TryParse(stringValue, out value))
+	{
+		if (value >= 0 && value <= 9)
+		{
+			return true;
+		}
+	}
+	Console.WriteLine($"You wrote incorrect value {stringValue}");
+	return false;
+}
+
+static void LINQ(int[] myarray, int number)
+{
+	var result = myarray.Reverse()
+					  .Distinct()
+					  .Where(x => x > 0 && x % 10 == number)
+					  .Reverse();
+	Console.WriteLine($"\n Result: ");
+	foreach (int i in result)
+	{
+		Console.Write(i + $"\t");
+	}
+}
+static void ActionsWithArray(int[] myarray)
+{
+	Console.WriteLine(" My array: ");
+	foreach (int number in myarray)
+	{
+		Console.Write(number + $"\t");
+	}
+}
